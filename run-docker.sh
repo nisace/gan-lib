0f6f649d760149dc7ed4c7e678ee251e368fe948
nisace <nisace.contact@gmail.com>
2017-02-03 11:15:03 +0100

    [DOCKER] - Add scripts to build and run Docker

--- run-docker.sh
+++ run-docker.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
-cp docker-compose.example.yml docker-compose.yml
-sed -i "s%/pwd%$PWD%g" docker-compose.yml
-docker-compose up -d
-docker exec -it ganlib_worker_1 bash
+cpu="cpu"
+gpu="gpu"
+if [ -z $1 ] || !([ $1 = $cpu ] || [ $1 = $gpu ])
+    then
+        echo "Please specify if you want to run Tensorflow for CPU or GPU."
+        echo "Usage:"
+        echo "  $0 [COMMAND]"
+        echo ""
+        echo "Commands:"
+        echo "  $cpu"
+        echo "  $gpu"
+        exit 1
+fi
+./prepare-docker-compose-yml.sh $1
+if [ $1 = $cpu ]
+    then
+        local_image="ganlibcpu"
+    else
+        local_image="ganlibgpu"
+fi
+docker-compose -p $local_image up -d
+docker exec -it ${local_image}_worker_1 bash
